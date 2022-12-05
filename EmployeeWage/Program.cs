10c6f7ccda7e3a8d8080cef59a52bd90666c6e6e
yashdhanvijay <yash10dhanvijay.com>
2022-12-05 19:35:38 +0530

    [Yash]:Attendance

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmployeeeWage
+
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            int fulltime = 1;
+            Random random = new Random();
+            int checkpresent = random.Next(0, 2);
+            Console.WriteLine("Random " + checkpresent);
+            if (checkpresent == fulltime)
+               {
+                Console.WriteLine("employee is present");
+               }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+
+        }
+
+    }
+
+}
